1740ba600ebca5954d701dabfb800c092bb5aa14
NAZ <fnwayalani@gmail.com>
2024-06-26 16:00:56 +0100

    first commit

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+const nameInput = document.getElementsByClassName("names-input");
+const numberInput = document.getElementByClassName("number-input");
+const monthInput = document.getElementByClassName("month-input");
+const yearInput = document.getElementByClassName("year-input");
+const cvcInput = document.getElementByClassName("cvc-input");
+const confirmInput = document.getElementById("confirm-input");
+const cc-number = document.getElementsByClassName("cc-number");
